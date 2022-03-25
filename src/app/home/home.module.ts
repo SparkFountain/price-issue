14dcbed6c0bb9c8ed5fcd4ea899febe83ac1a5fc
Stefan Behrendt <behrendt@onexip.com>
2022-03-25 12:20:36 +0100

    improved layout; minor spelling mistake fixes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { TranslocoModule } from '@ngneat/transloco';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, TranslocoModule, MatCardModule],
 })
-export class HomeModule { }
+export class HomeModule {}
